05d86028cc6d5fdd221bab4fe617cd31f7c3ae71
MosesChang <1234512345>
2020-07-07 23:09:59 +0800

    Wall can't move

--- src/states/main.state.ts
+++ src/states/main.state.ts
@@ -9,31 +9,49 @@ enum TileType {
 }
 
 class Tile {
-  sprite: Phaser.Sprite ;
-  type: TileType ;
-  constructor(id: TileType, sprite : Phaser.Sprite) {
+  private sprite: Phaser.Sprite ;
+  private game: Phaser.Game ;
+  private type: TileType ;
+  constructor(game: Phaser.Game, id: TileType, sprite : Phaser.Sprite) {
+    this.game = game ;
     this.sprite = sprite ;
+    this.sprite.anchor.setTo(0.5, 0.5) ;
     this.setTileType(id) ;
+    if (id === TileType.Wall) {
+      this.game.physics.p2.enable(this.sprite) ;
+      this.sprite.body.kinematic = true ;
+      this.sprite.body.static = true ;
+    }
   }
   public setTileType(id: TileType) {
     this.type = id ;
     this.sprite.frame = this.type ;
   }
+  public set visible(value: boolean) {
+    this.sprite.visible = value ;
+    if (this.type === TileType.Wall) {
+      if (value) {
+        this.sprite.body.addToWorld() ;
+      } else {
+        this.sprite.body.removeFromWorld() ;
+      }
+    }
+  }
+  public get visible(): boolean {
+    return this.sprite.visible ;
+  }
 }
 
 // The main state of the game
 export default class MainState extends State {
-  sky: Phaser.Sprite; // Reference to background sprite
-
   tiles : Tile[][] ;
   allTileIndex : Phaser.Point[] ;
   tankSprite: Phaser.Sprite ;
   tileGroup : Phaser.Group ;
   cursors: Phaser.CursorKeys ;
 
-  // const
-  tileWidth: number = 32 ;
-  visibleTileRadius: number = 5 ;
+  TILE_WIDTH: number = 32 ;
+  VISIBLE_TILE_RADIUS: number = 5 ;
 
   create(): void {
     // Init
@@ -49,6 +67,7 @@ export default class MainState extends State {
     this.tankSprite = this.game.add.sprite(0, 0, 'tankImage');
     this.tankSprite.anchor.setTo(0.5, 0.5) ;
     this.game.physics.p2.enable(this.tankSprite);
+    this.tankSprite.body.setCircle(15);
     this.tankSprite.body.fixedRotation = true;      // May be not!
     this.game.camera.follow(this.tankSprite, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
 
@@ -86,15 +105,15 @@ export default class MainState extends State {
   }
 
   private tileAssign() {
-    let totalRadius = this.tileWidth * this.visibleTileRadius ;
-    let leftBoundry = (this.tankSprite.position.x - totalRadius) / this.tileWidth | 0 ;
-    let rightBoundry = (this.tankSprite.position.x + totalRadius) / this.tileWidth | 0 ;
-    let topBoundry = (this.tankSprite.position.y - totalRadius) / this.tileWidth | 0 ;
-    let bottomBoundry = (this.tankSprite.position.y + totalRadius) / this.tileWidth | 0 ;
+    let totalRadius = this.TILE_WIDTH * this.VISIBLE_TILE_RADIUS ;
+    let leftBoundry = (this.tankSprite.position.x - totalRadius) / this.TILE_WIDTH | 0 ;
+    let rightBoundry = (this.tankSprite.position.x + totalRadius) / this.TILE_WIDTH | 0 ;
+    let topBoundry = (this.tankSprite.position.y - totalRadius) / this.TILE_WIDTH | 0 ;
+    let bottomBoundry = (this.tankSprite.position.y + totalRadius) / this.TILE_WIDTH | 0 ;
     for (let iX = leftBoundry ; iX <= rightBoundry ; iX ++) {
       for (let iY = topBoundry ; iY <= bottomBoundry ; iY ++) {
         if (!this.checkTileExistAnyArrayIt(iX, iY)) {
-          const tileSprite = this.game.add.sprite(iX * this.tileWidth, iY * this.tileWidth, 'tileImage', 0, this.tileGroup) ;
+          const tileSprite = this.game.add.sprite(iX * this.TILE_WIDTH, iY * this.TILE_WIDTH, 'tileImage', 0, this.tileGroup) ;
           let randID = this.game.rnd.integerInRange(0, 10) ;
           if (randID < 9) {
             randID = 0 ;
@@ -103,10 +122,10 @@ export default class MainState extends State {
           } else if (randID === 10) {
             randID = 2 ;
           }
-          this.tiles[iX][iY] = new Tile(randID, tileSprite) ;
+          this.tiles[iX][iY] = new Tile(this.game, randID, tileSprite) ;
           this.allTileIndex.push(new Phaser.Point(iX, iY)) ;
         } else {
-          this.tiles[iX][iY].sprite.visible = true ;
+          this.tiles[iX][iY].visible = true ;
         }
       }
     }
@@ -114,8 +133,8 @@ export default class MainState extends State {
     // invisivle out of boundry
     this.allTileIndex.forEach((value: Phaser.Point, index: number, array: Phaser.Point[]) => {
       if (value.x < leftBoundry || value.x > rightBoundry || value.y < topBoundry || value.y > bottomBoundry) {
-        if (this.tiles[value.x][value.y].sprite.visible === true) {
-          this.tiles[value.x][value.y].sprite.visible = false ;
+        if (this.tiles[value.x][value.y].visible === true) {
+          this.tiles[value.x][value.y].visible = false ;
         }
       }
     }) ;
