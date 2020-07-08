1453581e6cf439d6466c0654aa1b0866add10c28
MosesChang <1234512345>
2020-07-08 14:51:20 +0800

    Add buttons

--- src/states/main.state.ts
+++ src/states/main.state.ts
@@ -24,10 +24,12 @@ class Tile {
     this.setTileType(id) ;
     this.hp = this.ORIGIN_HP[this.type] ;
     if (id === TileType.Wall) {
+      // Wall add to physic
       this.game.physics.p2.enable(this.sprite) ;
       this.sprite.body.kinematic = true ;
       this.sprite.body.static = true ;
     } else if (id === TileType.Hay)  {
+      // Hay add effect
       this.effectSprite = this.game.add.sprite(0, 0, 'tileImage', id, effectGroup);
       this.effectSprite.position.setTo(this.sprite.position.x, this.sprite.position.y) ;
       this.effectSprite.anchor.setTo(0.5, 0.5) ;
@@ -93,9 +95,22 @@ export default class MainState extends State {
   private topBoundry : number;
   private bottomBoundry : number;
 
+  private aBt : Phaser.Button ;
+  private aPress : boolean = false ;
+  private bBt : Phaser.Button ;
+  private bPress : boolean = false ;
+  private rBt : Phaser.Button ;
+  private rPress : boolean = false ;
+  private lBt : Phaser.Button ;
+  private lPress : boolean = false ;
+  private uBt : Phaser.Button ;
+  private uPress : boolean = false ;
+  private dBt : Phaser.Button ;
+  private dPress : boolean = false ;
+
   private BULLET_DAMAGE = [10, 20, 25] ;
   private TANK_THRUST = 400 ;
-  private TANK_ROTATE = 100 ;
+  private TANK_ROTATE = 50 ;
   private BULLET_VELOCITY = 400 ;
   private TILE_WIDTH: number = 32 ;
   private VISIBLE_TILE_RADIUS: number = 15 ;    // test will be 5
@@ -129,28 +144,13 @@ export default class MainState extends State {
     this.tileAssign() ;
 
     // Bullet fire
-    this.game.input.keyboard.addKey(Phaser.KeyCode.ONE).onDown.add(() => {
-      if (!this.bulletSprite.visible) {
-        let recoil = (this.tankSprite.frame as number + 1) * 0.7 * -this.TANK_THRUST;
-        this.tankSprite.body.thrust(recoil) ;
-        const lootAt = new Phaser.Point( Math.cos(Phaser.Math.degToRad(this.tankSprite.body.angle - 90)), Math.sin(Phaser.Math.degToRad(this.tankSprite.body.angle - 90))) ;
-        this.bulletSprite.visible = true ;
-        this.bulletSprite.frame = this.tankSprite.frame ;
-        this.bulletSprite.position.setTo(this.tankSprite.position.x + (lootAt.x * 18), this.tankSprite.position.y + (lootAt.y * 18)) ;
-        this.bulletVelocity.x = lootAt.x * this.BULLET_VELOCITY ;
-        this.bulletVelocity.y = lootAt.y * this.BULLET_VELOCITY ;
-      }
-    });
+    this.game.input.keyboard.addKey(Phaser.KeyCode.ONE).onDown.add(() => this.bulletFire());
 
     // Tank color
-    this.game.input.keyboard.addKey(Phaser.KeyCode.TWO).onDown.add(() => {
-      let frameID = this.tankSprite.frame as number ;
-      frameID ++ ;
-      if (frameID > 2) {
-        frameID = 0 ;
-      }
-      this.tankSprite.frame = frameID ;
-    });
+    this.game.input.keyboard.addKey(Phaser.KeyCode.TWO).onDown.add(() => this.changeTankColor());
+
+    // Pad
+    this.initBts() ;
 
     // Cursor
     this.cursors = this.game.input.keyboard.createCursorKeys();
@@ -160,57 +160,29 @@ export default class MainState extends State {
     // input and move
     this.tankSprite.body.setZeroRotation();
     let move = false ;
-    if (this.cursors.up.isDown) {
+    if (this.cursors.up.isDown || this.uPress) {
       this.tankSprite.body.thrust(this.TANK_THRUST) ;
       move = true ;
-    } else if (this.cursors.down.isDown) {
+    } else if (this.cursors.down.isDown || this.dPress) {
       this.tankSprite.body.thrust(-this.TANK_THRUST) ;
       move = true ;
     }
-    if (this.cursors.left.isDown) {
+    if (this.cursors.left.isDown || this.lPress) {
       this.tankSprite.body.rotateLeft(this.TANK_ROTATE);
-      move = true ;
-    } else if (this.cursors.right.isDown) {
+    } else if (this.cursors.right.isDown || this.rPress) {
       this.tankSprite.body.rotateRight(this.TANK_ROTATE);
-      move = true ;
     }
 
     // Bullet move & collision
-    if (this.bulletSprite.visible) {
-      this.bulletSprite.position.setTo(this.bulletSprite.position.x + (this.bulletVelocity.x * this.game.time.elapsed / 1000),
-        this.bulletSprite.position.y + (this.bulletVelocity.y * this.game.time.elapsed / 1000)) ;
-      let pos = this.bulletSprite.position ;
-      // Boundry check
-      if (pos.x >= this.rightBoundry * this.TILE_WIDTH || pos.x <= this.leftBoundry * this.TILE_WIDTH ||
-        pos.y >= this.bottomBoundry * this.TILE_WIDTH || pos.y <= this.topBoundry * this.TILE_WIDTH) {
-        this.bulletSprite.visible = false ;
-      } else {
-        let xless = (this.bulletSprite.position.x / this.TILE_WIDTH | 0) - 2  ;
-        let xMore = (this.bulletSprite.position.x / this.TILE_WIDTH | 0) + 2  ;
-        let yless = (this.bulletSprite.position.y / this.TILE_WIDTH | 0) - 2  ;
-        let yMore = (this.bulletSprite.position.y / this.TILE_WIDTH | 0) + 2  ;
-        for (let iX = xless ; iX <= xMore ; iX++) {
-          for (let iY = yless ; iY <= yMore ; iY++) {
-            if (iX < this.leftBoundry || iX > this.rightBoundry || iY < this.topBoundry || iY > this.bottomBoundry) {
-              continue ;
-            }
-            if (this.tiles[iX][iY].type !== TileType.Floor) {
-              if (this.tiles[iX][iY].ckeckCollision(this.bulletSprite.position)) {
-                this.bulletSprite.visible = false ;
-                // Hit hey
-                if (this.tiles[iX][iY].type === TileType.Hay) {
-                  this.tiles[iX][iY].hitBullet(this.BULLET_DAMAGE[this.bulletSprite.frame]) ;
-                }
-                break ;
-              }
-            }
-          }
-          if (this.bulletSprite.visible === false) {
-            break ;
-          }
-        }
-      }
-    }
+    this.bulletMoveCollision() ;
+
+    // Pad position follow camera
+    this.aBt.position.setTo(this.game.camera.position.x + 580, this.game.camera.position.y + 460) ;
+    this.bBt.position.setTo(this.game.camera.position.x + 680, this.game.camera.position.y + 460) ;
+    this.rBt.position.setTo(this.game.camera.position.x + 500, this.game.camera.position.y + 478) ;
+    this.lBt.position.setTo(this.game.camera.position.x + 380, this.game.camera.position.y + 478) ;
+    this.uBt.position.setTo(this.game.camera.position.x + 445, this.game.camera.position.y + 410) ;
+    this.dBt.position.setTo(this.game.camera.position.x + 445, this.game.camera.position.y + 532) ;
 
     // Tile assign (create, show & hide)
     if (move) {
@@ -267,6 +239,165 @@ export default class MainState extends State {
       isExist = false ;
     }
     return isExist ;
+  }
+
+  private bulletMoveCollision() {
+    if (this.bulletSprite.visible) {
+      // Bullet move
+      this.bulletSprite.position.setTo(this.bulletSprite.position.x + (this.bulletVelocity.x * this.game.time.elapsed / 1000),
+        this.bulletSprite.position.y + (this.bulletVelocity.y * this.game.time.elapsed / 1000)) ;
+      let pos = this.bulletSprite.position ;
+      // Boundry check
+      if (pos.x >= this.rightBoundry * this.TILE_WIDTH || pos.x <= this.leftBoundry * this.TILE_WIDTH ||
+        pos.y >= this.bottomBoundry * this.TILE_WIDTH || pos.y <= this.topBoundry * this.TILE_WIDTH) {
+        this.bulletSprite.visible = false ;
+      } else {
+        let xless = (this.bulletSprite.position.x / this.TILE_WIDTH | 0) - 2  ;
+        let xMore = (this.bulletSprite.position.x / this.TILE_WIDTH | 0) + 2  ;
+        let yless = (this.bulletSprite.position.y / this.TILE_WIDTH | 0) - 2  ;
+        let yMore = (this.bulletSprite.position.y / this.TILE_WIDTH | 0) + 2  ;
+        for (let iX = xless ; iX <= xMore ; iX++) {
+          for (let iY = yless ; iY <= yMore ; iY++) {
+            if (iX < this.leftBoundry || iX > this.rightBoundry || iY < this.topBoundry || iY > this.bottomBoundry) {
+              continue ;
+            }
+            if (this.tiles[iX][iY].type !== TileType.Floor) {
+              if (this.tiles[iX][iY].ckeckCollision(this.bulletSprite.position)) {
+                this.bulletSprite.visible = false ;
+                // Hit hey
+                if (this.tiles[iX][iY].type === TileType.Hay) {
+                  this.tiles[iX][iY].hitBullet(this.BULLET_DAMAGE[this.bulletSprite.frame]) ;
+                }
+                break ;
+              }
+            }
+          }
+          if (this.bulletSprite.visible === false) {
+            break ;
+          }
+        }
+      }
+    }
+  }
+
+  private initBts() {
+    this.aPress = false ;
+    this.aBt = this.game.add.button(0, 0, 'xbox360', undefined, this, '360_A', '360_A', '360_A');
+    this.aBt.alpha = 0.5 ;
+    this.aBt.events.onInputDown.add(() => {
+      this.aPress = true; this.aBt.alpha = 1;
+      this.bulletFire();
+    }, this);
+    this.aBt.events.onInputOut.add(() => {
+      this.aPress = false;
+      this.aBt.alpha = 0.5;
+    }, this);
+    this.aBt.events.onInputUp.add(() => {
+      this.aPress = false;
+      this.aBt.alpha = 0.5;
+    }, this);
+
+    this.bPress = false ;
+    this.bBt = this.game.add.button(0, 0, 'xbox360', undefined, this, '360_B', '360_B', '360_B');
+    this.bBt.alpha = 0.5 ;
+    this.bBt.events.onInputDown.add(() => {
+      this.bPress = true; this.bBt.alpha = 1;
+      this.changeTankColor();
+    }, this);
+    this.bBt.events.onInputOut.add(() => {
+      this.bPress = false;
+      this.bBt.alpha = 0.5;
+    }, this);
+    this.bBt.events.onInputUp.add(() => {
+      this.bPress = false;
+      this.bBt.alpha = 0.5;
+    }, this);
+
+    this.rPress = false ;
+    this.rBt = this.game.add.button(0, 0, 'rBtImage', undefined, this, 0, 0, 0);
+    this.rBt.alpha = 0.5 ;
+    this.rBt.events.onInputDown.add(() => {
+      this.rPress = true;
+      this.rBt.alpha = 1;
+    }, this);
+    this.rBt.events.onInputOut.add(() => {
+      this.rPress = false;
+      this.rBt.alpha = 0.5;
+    }, this);
+    this.rBt.events.onInputUp.add(() => {
+      this.rPress = false;
+      this.rBt.alpha = 0.5;
+    }, this);
+
+    this.lPress = false ;
+    this.lBt = this.game.add.button(0, 0, 'lBtImage', undefined, this, 0, 0, 0);
+    this.lBt.alpha = 0.5 ;
+    this.lBt.events.onInputDown.add(() => {
+      this.lPress = true;
+      this.lBt.alpha = 1;
+    }, this);
+    this.lBt.events.onInputOut.add(() => {
+      this.lPress = false;
+      this.lBt.alpha = 0.5;
+    }, this);
+    this.lBt.events.onInputUp.add(() => {
+      this.lPress = false;
+      this.lBt.alpha = 0.5;
+    }, this);
+
+    this.uPress = false ;
+    this.uBt = this.game.add.button(0, 0, 'uBtImage', undefined, this, 0, 0, 0);
+    this.uBt.alpha = 0.5 ;
+    this.uBt.events.onInputDown.add(() => {
+      this.uPress = true;
+      this.uBt.alpha = 1;
+    }, this);
+    this.uBt.events.onInputOut.add(() => {
+      this.uPress = false;
+      this.uBt.alpha = 0.5;
+    }, this);
+    this.uBt.events.onInputUp.add(() => {
+      this.uPress = false;
+      this.uBt.alpha = 0.5;
+    }, this);
+
+    this.dPress = false ;
+    this.dBt = this.game.add.button(0, 0, 'dBtImage', undefined, this, 0, 0, 0);
+    this.dBt.alpha = 0.5 ;
+    this.dBt.events.onInputDown.add(() => {
+      this.dPress = true;
+      this.dBt.alpha = 1;
+    }, this);
+    this.dBt.events.onInputOut.add(() => {
+      this.dPress = false;
+      this.dBt.alpha = 0.5;
+    }, this);
+    this.dBt.events.onInputUp.add(() => {
+      this.dPress = false;
+      this.dBt.alpha = 0.5;
+    }, this);
+  }
+
+  private bulletFire() {
+    if (!this.bulletSprite.visible) {
+      let recoil = (this.tankSprite.frame as number + 1) * 0.9 * -this.TANK_THRUST;
+      this.tankSprite.body.thrust(recoil) ;
+      const lootAt = new Phaser.Point( Math.cos(Phaser.Math.degToRad(this.tankSprite.body.angle - 90)), Math.sin(Phaser.Math.degToRad(this.tankSprite.body.angle - 90))) ;
+      this.bulletSprite.visible = true ;
+      this.bulletSprite.frame = this.tankSprite.frame ;
+      this.bulletSprite.position.setTo(this.tankSprite.position.x + (lootAt.x * 18), this.tankSprite.position.y + (lootAt.y * 18)) ;
+      this.bulletVelocity.x = lootAt.x * this.BULLET_VELOCITY ;
+      this.bulletVelocity.y = lootAt.y * this.BULLET_VELOCITY ;
+    }
+  }
+
+  private changeTankColor() {
+    let frameID = this.tankSprite.frame as number ;
+    frameID ++ ;
+    if (frameID > 2) {
+      frameID = 0 ;
+    }
+    this.tankSprite.frame = frameID ;
   }
 
 }
--- src/states/preloader.state.ts
+++ src/states/preloader.state.ts
@@ -3,6 +3,12 @@
 import State from './state';
 
 // Webpack will replace these imports with a URLs to images
+const xbox360Image    = require('assets/images/xbox360.png');
+const xbox360Json     = require('assets/images/xbox360.json');
+const rBtImage       = require('assets/images/rBt.png');
+const uBtImage       = require('assets/images/uBt.png');
+const lBtImage       = require('assets/images/lBt.png');
+const dBtImage       = require('assets/images/dBt.png');
 const tileImage       = require('assets/images/tile.png');
 const tankImage       = require('assets/images/tank.png');
 const bulletImage     = require('assets/images/bullet.png');
@@ -12,6 +18,11 @@ export default class PreloaderState extends State {
   preload(): void {
     console.debug('Assets loading started');
 
+    this.game.load.atlas('xbox360', xbox360Image, xbox360Json);
+    this.game.load.image('rBtImage', rBtImage) ;
+    this.game.load.image('uBtImage', uBtImage) ;
+    this.game.load.image('lBtImage', lBtImage) ;
+    this.game.load.image('dBtImage', dBtImage) ;
     this.game.load.spritesheet('tileImage', tileImage, 32, 32);
     this.game.load.spritesheet('tankImage', tankImage, 31, 40);
     this.game.load.spritesheet('bulletImage', bulletImage, 17, 17);
